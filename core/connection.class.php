f89f721f2b3d2c85e08b6bc7232deaa840267a21
vasil1960 <v.tsigov@gmail.com>
2017-06-11 20:33:28 +0300

    Edit some files

--- core/connection.class.php
+++ core/connection.class.php
@@ -5,7 +5,7 @@ class Connection {
 	public static function make()
 	{
 		try {
-			return new PDO('mysql:host=172.16.4.34;dbname=nugEmpl;charset=utf8','','');
+			return new PDO('mysql:host=172.16.4.34;dbname=nugEmpl;charset=utf8','cotaivo','taniami');
 			 
 		} catch (PDOException $e) {
 			die($e->getMessage());	
